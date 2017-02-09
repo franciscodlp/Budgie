78b1fbee8b43f9451dedb39953d4354ae6a835f3
Francisco de la Pena <f@twisterlabs.io>
2017-02-09 01:21:34 -0800

    Add Timeline Cells
    - Cells communicate actions to the view controller via Delegation.
    - Most methods are setters that configure different elements of the cell
    based on current state of properties.
    - There is no functionality on the cell beside changing icon and increasing counters
    when tapping in the cell buttons.

--- Budgie/Home/HomeTweetCell.swift
+++ Budgie/Home/HomeTweetCell.swift
@@ -8,17 +8,122 @@
 
 import UIKit
 
+protocol HomeTweetCellDelegate: class {
+  func onShareButton()
+  func onRetweetButton()
+  func onLikeButton()
+}
+
 class HomeTweetCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+  @IBOutlet var profileImageView: UIImageView!
+  @IBOutlet var nameLabel: UILabel!
+  @IBOutlet var handlerLabel: UILabel!
+  @IBOutlet var dateLabel: UILabel!
+  @IBOutlet var contentLabel: UILabel!
+  @IBOutlet var shareButton: UIButton!
+  @IBOutlet var retweetButton: UIButton!
+  @IBOutlet var likeButton: UIButton!
+
+  var shareCounter: Double! = 0.0 {
+    didSet {
+      shareButton.setTitle(String(format: "%.0f", shareCounter), for: .normal)
+    }
+  }
+
+  var retweetsCounter: Double! = 0.0 {
+    didSet {
+      retweetButton.setTitle(String(format: "%.0f", retweetsCounter), for: .normal)
+    }
+  }
+
+  var likesCounter: Double! = 0.0 {
+    didSet {
+      likeButton.setTitle(String(format: "%.0f", likesCounter), for: .normal)
+    }
+  }
+
+  var isRetweeted: Bool! = false {
+    didSet {
+      let image = isRetweeted! ? k.retweetIconSelected : k.retweetIcon
+      retweetButton.setImage(image, for: .normal)
+    }
+  }
+  var isFavourite: Bool! = false {
+    didSet {
+      let image = isFavourite! ? k.likeIconSelected : k.likeIconSolid
+      likeButton.setImage(image, for: .normal)
+    }
+  }
+
+  @IBAction func onShareButton(_ sender: UIButton) {
+    defer {
+      delegate?.onShareButton()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    guard let counterString = shareButton.titleLabel?.text else {
+      shareButton.setTitle("1", for: .normal)
+      return
     }
+
+    shareButton.setTitle(String(format: "%.0f", Double(counterString)! + 1), for: .normal)
+    return
+  }
+
+  @IBAction func onRetweetButton(_ sender: UIButton) {
+    defer {
+      delegate?.onRetweetButton()
+      isRetweeted = !isRetweeted
+    }
+
+    guard let counterString = retweetButton.titleLabel?.text else {
+      retweetButton.setTitle("1", for: .normal)
+      retweetButton.setImage(k.retweetIconSelected, for: .normal)
+      return
+    }
+
+    if isRetweeted! {
+      retweetButton.setTitle(String(format: "%.0f", Double(counterString)! - 1), for: .normal)
+      retweetButton.setImage(k.retweetIcon, for: .normal)
+    } else {
+      retweetButton.setTitle(String(format: "%.0f", Double(counterString)! + 1), for: .normal)
+      retweetButton.setImage(k.retweetIconSelected, for: .normal)
+    }
+
+    return
+  }
+
+  @IBAction func onLikeButton(_ sender: UIButton) {
+    defer {
+      delegate?.onLikeButton()
+      isFavourite = !isFavourite
+    }
+
+    guard let counterString = likeButton.titleLabel?.text else {
+      likeButton.setTitle("1", for: .normal)
+      likeButton.setImage(k.likeIconSelected, for: .normal)
+      return
+    }
+
+    if isFavourite! {
+      likeButton.setTitle(String(format: "%.0f", Double(counterString)! - 1), for: .normal)
+      likeButton.setImage(k.likeIconSolid, for: .normal)
+    } else {
+      likeButton.setTitle(String(format: "%.0f", Double(counterString)! + 1), for: .normal)
+      likeButton.setImage(k.likeIconSelected, for: .normal)
+    }
+
+    return
+  }
+
+  weak var delegate: HomeTweetCellDelegate?
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    self.selectionStyle = .none
+    profileImageView.clipsToBounds = true
+    profileImageView.contentMode = .scaleAspectFill
+
+  }
 
 }
